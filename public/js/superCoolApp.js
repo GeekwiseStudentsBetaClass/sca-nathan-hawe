c6ac80079bc38b44a24506c3855026cee5137001
Nathan Hawe <nathanhawe@users.noreply.github.com>
2016-06-22 20:07:25 -0700

    Restructured ui-router routes

--- public/js/superCoolApp.js
+++ public/js/superCoolApp.js
@@ -3,7 +3,7 @@
 
     app.config(function($stateProvider, $urlRouterProvider){
         $urlRouterProvider.otherwise('home');
-        $urlRouterProvider.when('/login', '/login/go');
+        
         $stateProvider
             .state('home', {
                 url: '/home',
@@ -12,16 +12,11 @@
             .state('login', {
                 url: '/login',
                 templateUrl: 'views/login.html',
-            })
-            /* login child states */
-            .state('login.go', {
-                url: '/go',
-                templateUrl: 'views/login.go.html',
                 controller:  'LoginController as loginCtrl'
             })
-            .state('login.register', {
+            .state('register', {
                 url: '/register',
-                templateUrl: 'views/login.register.html',
+                templateUrl: 'views/register.html',
                 controller: 'RegistrationController as registrationCtrl'
             })
     })
