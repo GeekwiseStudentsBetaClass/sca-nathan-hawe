760097a9bd216b53d11be16762d93d803a060847
Nathan Hawe <nathanhawe@users.noreply.github.com>
2016-06-22 19:15:08 -0700

    Split the login and registration controllers into their own separate nested views.

--- public/js/superCoolApp.js
+++ public/js/superCoolApp.js
@@ -3,6 +3,7 @@
 
     app.config(function($stateProvider, $urlRouterProvider){
         $urlRouterProvider.otherwise('home');
+        $urlRouterProvider.when('/login', '/login/go');
         $stateProvider
             .state('home', {
                 url: '/home',
@@ -10,7 +11,18 @@
             })
             .state('login', {
                 url: '/login',
-                templateUrl: 'views/login.html'
+                templateUrl: 'views/login.html',
+            })
+            /* login child states */
+            .state('login.go', {
+                url: '/go',
+                templateUrl: 'views/login.go.html',
+                controller:  'LoginController as loginCtrl'
+            })
+            .state('login.register', {
+                url: '/register',
+                templateUrl: 'views/login.register.html',
+                controller: 'RegistrationController as registrationCtrl'
             })
     })
 })();
